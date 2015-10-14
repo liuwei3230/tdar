fecc1ba9622a2fd718bde1e6b8938d70f1388def
Jim deVos <james.t.devos@asu.edu>
2015-10-13 20:52:44 -0700

    adding excelprocessor test

---
+++ core/src/test/java/org/tdar/core/service/excel/template/ExcelTemplateProcessorTest.java
@@ -0,0 +1,57 @@
+package org.tdar.core.service.excel.template;
+
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tdar.core.bean.entity.Person;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+
+/**
+ * Created by jim on 10/13/15.
+ */
+public class ExcelTemplateProcessorTest {
+
+    File targetDir;
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Before
+    public void beforeEach() {
+        targetDir = Paths.get("target/generated/excel").toFile();
+        logger.debug("target: {}", targetDir.getAbsolutePath());
+
+        if (!targetDir.exists()){
+            targetDir.mkdirs();
+        }
+    }
+
+    @After
+    public void afterEach() {
+    }
+
+
+
+    @Test
+    public void testSimpleOutput() throws IOException {
+        ExcelTemplateProcessor processor = new ExcelTemplateProcessor();
+        File outfile = File.createTempFile("foo", ".xls", targetDir);
+
+        List<String> headers = Arrays.asList("col1, col2, col3");
+        List<Person> data = new ArrayList<Person>() {{
+            add(new Person("jim", "devos", "email1"));
+            add(new Person("john", "doe", "email2"));
+            add(new Person("jane", "doe", "email3"));
+        }};
+
+        List<String> names = Arrays.asList("firstName", "lastName", "email");
+
+        OutputStream os = new FileOutputStream(outfile);
+        processor.process(headers, data, names, os);
+
+        // TODO: use POI to open the resultant file and slurp the data back in, then verify w/ data in test
+    }
+
+}
