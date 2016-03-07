11d49d9d1dca0d6048041e007a6b9bd06ecf529e
Jim deVos <james.t.devos@asu.edu>
2016-03-06 17:25:26 -0700

    more work on xls templates

--- core/src/main/java/org/tdar/core/service/excel/ExcelTemplateProcessor.java
+++ core/src/main/java/org/tdar/core/service/excel/ExcelTemplateProcessor.java
@@ -1,11 +1,15 @@
 package org.tdar.core.service.excel;
 
+import org.apache.commons.lang3.StringUtils;
+import org.jxls.common.Context;
 import org.jxls.template.SimpleExporter;
+import org.jxls.util.JxlsHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -15,7 +19,6 @@ public class ExcelTemplateProcessor {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    SimpleExporter exporter;
 
     /**
      * Instantiate template processor with a default template.
@@ -24,35 +27,28 @@ public class ExcelTemplateProcessor {
      *
      */
     public ExcelTemplateProcessor() {
-        exporter = new SimpleExporter();
     }
 
-    /**
-     * Instantiate template processor with specified JXLS template.
-     *
-     * @param template JXLS to that this processor will apply when rendering an excel
-     *                 spreadsheet.
-     *
-     * @see <a href="http://jxls.sourceforge.net/reference/excel_markup.html">JXLS: Excel mark-up</a>
-     */
-    public ExcelTemplateProcessor(File template) throws IOException{
-        this();
-        try (InputStream is = new FileInputStream(template)) {
-            exporter.registerGridTemplate(is);
+
+    public void process(List<String> headers, List<?> data, List<String> propertyNames, OutputStream xlsStream ) {
+        String joinedNames = StringUtils.join(propertyNames, ", ");
+        SimpleExporter exporter = new SimpleExporter();
+        exporter.gridExport(headers, data, joinedNames, xlsStream);
+    }
+
+
+    public void process(File template, Map<String, Object> data, OutputStream xlsStream) throws IOException {
+        Context context = new Context(data);
+        try(InputStream inputStream = new FileInputStream(template)){
+            JxlsHelper.getInstance().processTemplate(inputStream, xlsStream, context);
         }
     }
 
-    public void process(List<String> headers, List<?> data, List<String> propertyNames, OutputStream xlsStream ) {
 
-        //String joinedNames = StringUtils.join(propertyNames, ", ");
-
-        //The power of Java 8 turns one line into four!!!
-        String joinedNames = propertyNames
-                .stream()
-                .reduce( (t, u) -> t + ", " + u)
-                .get();
-
-        exporter.gridExport(headers, data, joinedNames, xlsStream);
+    public void process(File template, Map<String, Object> data, File outfile) throws IOException {
+        try(OutputStream os = new FileOutputStream(outfile)) {
+            process(template, data, os);
+        }
     }
 
 }
--- core/src/test/java/org/tdar/core/service/excel/ExcelTemplateProcessorTest.java
+++ core/src/test/java/org/tdar/core/service/excel/ExcelTemplateProcessorTest.java
@@ -1,7 +1,6 @@
 package org.tdar.core.service.excel;
 
 import org.junit.*;
-import org.jxls.util.JxlsHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tdar.TestConstants;
@@ -14,25 +13,19 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import org.jxls.common.Context;
-
 import static org.tdar.TestConstants.TEST_JXLS_TEMPLATE_DIR;
 
 /**
  * Tests for basic template processor
  */
+// TODO: use ExcelUnit to assert contents of generated files
 public class ExcelTemplateProcessorTest {
 
-    File targetDir;
-    File templateDir;
     Logger logger = LoggerFactory.getLogger(getClass());
+    File targetDir = Paths.get(System.getProperty("xlsdir", TestConstants.TEST_JXLS_DEST_DIR)).toFile();
+    File templateDir = Paths.get(System.getProperty("xlstemplatedir", TEST_JXLS_TEMPLATE_DIR)).toFile();;
+    ExcelTemplateProcessor templateProcessor = new ExcelTemplateProcessor();
 
-    public ExcelTemplateProcessorTest() {
-        targetDir = Paths.get(System.getProperty("xlsdir", TestConstants.TEST_JXLS_DEST_DIR)).toFile();
-
-        //FIXME: the maven process-resources phase isn't copying the jxjs-template dir to target/test-resources.
-        templateDir = Paths.get(System.getProperty("xlstemplatedir", TEST_JXLS_TEMPLATE_DIR)).toFile();
-    }
 
     void mkdirs(File dir) {
         if(!dir.exists()) {
@@ -41,76 +34,78 @@ public class ExcelTemplateProcessorTest {
         }
     }
 
+    //todo: this map initializer is handy.   consider promoting, at least to other tests
+    // return a Map.Entry
+    static <K, V> Map.Entry<K, V> entry(K key, V value) {
+        return new AbstractMap.SimpleEntry<>(key, value);
+    }
+
+    // return immutable map from an array of Map.Entry objects (do not insert nulls, okay?).
+    static <K, U> Map<K, U> newMap(Map.Entry<K, U>... entries) {
+        return Stream.of(entries).collect(Collectors.toMap((e) -> e.getKey(), (e) -> e.getValue()));
+    }
+
+
     @Before
     public void beforeEach() {
         mkdirs(targetDir);
         mkdirs(templateDir);
     }
 
-
-    @Test
-    public void testSimpleOutput() throws IOException {
-        ExcelTemplateProcessor processor = new ExcelTemplateProcessor();
-        File outfile = File.createTempFile("foo", ".xls", targetDir);
-
-        List<String> headers = Arrays.asList("FirstName", "LastName", "Email");
-        List<Person> data = new ArrayList<Person>() {{
-            add(new Person("jim", "devos", "email1"));
-            add(new Person("john", "doe", "email2"));
-            add(new Person("jane", "doe", "email3"));
-        }};
-
-        List<String> names = Arrays.asList("firstName", "lastName", "email");
-
-        OutputStream os = new FileOutputStream(outfile);
-        processor.process(headers, data, names, os);
-
-        // TODO: use POI to open the resultant file and slurp the data back in, then verify w/ data in test
-    }
-
-
-    @Test
-    public void testTemplate() throws IOException {
-        File outfile = new File(targetDir, "object_collection_output.xls");
-
-        List<Employee> employees = generateSampleEmployeeData(20);
-        try(InputStream is = new FileInputStream(new File(TEST_JXLS_TEMPLATE_DIR, "object_collection_template.xls"))) {
-            try (OutputStream os = new FileOutputStream(outfile)) {
-                Context context = new Context();
-                context.putVar("employees", employees);
-                JxlsHelper.getInstance().processTemplate(is, os, context);
-            }
-        }
-
-    }
-
-    @Test
-    public void testSearchResultsTemplate() throws IOException {
-        File outfile = new File(targetDir, "search_results_output.xls");
-
-        List<Employee> employees = generateSampleEmployeeData(20);
-        try(InputStream is = new FileInputStream(new File(TEST_JXLS_TEMPLATE_DIR, "search_results_template.xls"))) {
-            try (OutputStream os = new FileOutputStream(outfile)) {
-                Context context = new Context();
-                context.putVar("employees", employees);
-                context.putVar("info", new HashMap(){{
-                    put("dateCreated", new Date(0));
-                    put("url", "http://www.tdar.org");
-                    put("description", "hello mom!");
-                }});
-                JxlsHelper.getInstance().processTemplate(is, os, context);
-            }
-        }
-
-    }
-
-
     private List<Employee> generateSampleEmployeeData(int size) {
+        //Pretty sure a for-loop would have been easier here too, but who cares.  Onward!!
         return IntStream
                 .rangeClosed(0, size - 1)
                 .boxed()
                 .map((i) -> Employee.randomEmployee())
                 .collect(Collectors.toList());
+    }
+
+
+    @Test
+    public void testSimpleOutput() throws IOException {
+        File outfile = new File(targetDir, "test-simple-output.xls");
+        List<String> headers = Arrays.asList("FirstName", "LastName", "Email");
+
+        List<String>  colnames = Arrays.asList("firstName", "lastName", "email");
+        List<Person> rows = new ArrayList<Person>() {{
+            add(new Person("jim", "devos", "email1"));
+            add(new Person("john", "doe", "email2"));
+            add(new Person("jane", "doe", "email3"));
+        }};
+
+        OutputStream os = new FileOutputStream(outfile);
+        templateProcessor.process(headers, rows, colnames, os);
+    }
+
+
+    @Test
+    // essentially the same as jxls website example, adapted to use our wrapper function
+    public void testSampleTemplate() throws IOException {
+        File outfile = new File(targetDir, "object_collection_output.xls");
+        File template = new File(templateDir,"object_collection_template.xls");
+        Map<String, Object> data = newMap(entry("employees", generateSampleEmployeeData(20)));
+        templateProcessor.process(template, data, outfile);
+    }
+
+    @Test
+    public void testSearchResultsTemplate() throws IOException {
+        File template = new File(templateDir, "search_results_template.xls");
+        File outfile = new File(targetDir, "search_results_output.xls");
+        Map<String, Object> context = newMap(
+                entry("employees", generateSampleEmployeeData(20)),
+                entry("info", newMap(
+                        entry("dateCreated", new Date(0)),
+                        entry("url", "http://www.tdar.org"),
+                        entry("description", "this is a test")
+                )));
+        templateProcessor.process(template, context, outfile);
+    }
+
+    @Test
+    //process template that has data streams
+    public void testTemplateWithIterableData() {
+
     }
 
 }
